2347d88dce275f5116e6247b788aa1fc3bf25284
quirinof <matheusquinoff@gmail.com>
2024-09-15 12:56:46 -0300

    add funcionalidade de carregar e salvar metas

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const { select, input, checkbox } = require('@inquirer/prompts') 
+const fs = require("fs").promises
 
 let mensagem = "Bem vindo ao App de Metas"
 
-let meta = {
-  value: 'Tomar 3L de água por dia',
-  checked: false,
+let metas
+
+const carregarMetas = async () => {
+	try {
+		const dados = await fs.readFile("metas.json", "utf-8")
+		metas = JSON.parse(dados)
+	} catch (error) {
+		metas = []
+		mensagem = "Não foi possível carregar os dados."	
+	}
 }
 
-let metas = [ meta ]
+const salvarMetas =  async () => {
+	await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
 
 const cadastrarMeta = async () => {
 	const meta = await input({ message: "Digite a meta:"})
@@ -119,9 +129,11 @@ const mostrarMensagem = () => {
 }
 
 const start = async () => {
-    
+    await carregarMetas()
+
     while(true){
         mostrarMensagem()
+		await salvarMetas()
 
         const opcao = await select({
             message: "Menu >",
